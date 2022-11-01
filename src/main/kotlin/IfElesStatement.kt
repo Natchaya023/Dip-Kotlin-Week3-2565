c1b2b58e5894ccad8c6ac5861bf10fea11c71715
Natchaya023 <sakoleek93@gmail.com>
2022-11-01 17:13:05 +0700

    UP

---
+++ src/main/kotlin/IfElesStatement.kt
@@ -0,0 +1,14 @@
+fun IfEles (){
+    val number = 29
+
+    if(number%2 == 0){
+        print("Even number")
+    }
+
+    if(number%2 == 0){
+        print("Even number")
+    }
+    else{
+        print("Odd number")
+    }
+}
